c6793d4656dd667fdade34fec3e6ba20692d66f1
Lane <es.martinez@uniandes.edu.co>
2026-04-05 15:47:20 -0500

    **prueba**

--- Entrega 2/src/boardGameCafe/Main.java
+++ Entrega 2/src/boardGameCafe/Main.java
@@ -2,13 +2,8 @@ package boardGameCafe;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
