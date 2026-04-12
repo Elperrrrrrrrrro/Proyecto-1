0c35772827364ce2705b060ee77d28894b65d18a
Elperrrrrrrrrro <dg.arias@uniandes.edu.co>
2026-04-12 17:28:18 -0500

    cambios en Mesa en sistema boar adicionar funciones y en venta primer vistaso

--- Entrega 2/src/boardGameCafe/Mesa.java
+++ Entrega 2/src/boardGameCafe/Mesa.java
@@ -1,5 +1,6 @@
 package boardGameCafe;
 import java.util.*;
+import java.time.*;
 public class Mesa {
 	private int numeroMesa;
 	private Cliente clienteActual;
@@ -8,15 +9,18 @@ public class Mesa {
 	private int cantidadPersonas;
 	private boolean hayMenores;
 	private ArrayList<String> alergenos;
+	private boolean hayInfantes;
 	
-	public Mesa(int numero, int cantidad, boolean menores, ArrayList<String> alergenos, Cliente cliente) {
+	public Mesa(int numero, int cantidad, boolean menores, ArrayList<String> alergenos,boolean hayInfantes) {
 		this.numeroMesa= numero;
 		this.cantidadPersonas = cantidad;
 		this.hayMenores = menores; 
 		this.alergenos = alergenos;
 		this.pedidoActual = new ArrayList<>();
 		this.prestamoActicos = new ArrayList<>();
-		this.clienteActual = cliente;
+		this.clienteActual = null;
+		this.hayInfantes = hayInfantes;
+
 		
 	}
 	public int getNumeroMesa() {
@@ -43,6 +47,52 @@ public class Mesa {
 	
 	public void agregarAlPedido(ProductoMenu producto ) {
 		this.pedidoActual.add(producto);
+	}
+	public boolean isHayInfantes() {
+		return hayInfantes;
+	}
+	public void setClienteActual(Cliente clienteActual) {
+		this.clienteActual = clienteActual;
+	}
+	
+	
+	public ArrayList<String> liberarMesa() {
+		
+		this.cantidadPersonas=0;
+		this.clienteActual= null;
+		this.pedidoActual = new ArrayList<>();
+		this.prestamoActicos = new ArrayList<>();
+		this.hayInfantes= false;
+		this.hayMenores= false;
+		ArrayList<String> codigosJuegos = new ArrayList<>();
+
+
+		return codigosJuegos;
+		
+		
+	}
+	
+	public ArrayList<ProductoMenu> getPedidoActual() {
+		return pedidoActual;
+	}
+	public void setNumeroMesa(int numeroMesa) {
+		this.numeroMesa = numeroMesa;
+	}
+	public void setCantidadPersonas(int cantidadPersonas) {
+		this.cantidadPersonas = cantidadPersonas;
+	}
+	public void setHayMenores(boolean hayMenores) {
+		this.hayMenores = hayMenores;
+	}
+	public void setHayInfantes(boolean hayInfantes) {
+		this.hayInfantes = hayInfantes;
+	}
+	public void setAlergenos(ArrayList<String> alergenos) {
+		this.alergenos = alergenos;
+	}
+	
+	public void AgregarPrestamo(Prestamo prestamo) {
+		this.prestamoActicos.add(prestamo);
 	}
 	
 
--- Entrega 2/src/boardGameCafe/SistemaBoardGameCafe.java
+++ Entrega 2/src/boardGameCafe/SistemaBoardGameCafe.java
@@ -1,5 +1,101 @@
 package boardGameCafe;
 
-public class SistemaBoardGameCafe {
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.*;
 
+import java.time.*;
+
+public class SistemaBoardGameCafe {
+	private Map<String, juegoMesa> inventario; // llave id del juego
+	private Map<String, Empleado> empleados; // llave es el login+password de los empleados
+	private Map<String, Cliente> clientes; // llave documento clientes
+	private Map<String, Mesa> mesas; //llave numero mesa 
+	private Map<String, Turno> turnos; // llave dia de la semana 
+	private Map<String, Venta>  historialVenta ; // la llave es el ID venta
+	private Map<String, PrestamoCliente> historailPrestamosClientes;
+	private Map<String, PrestamoEmpleado> historailPrestamosEmpleados;
+	private ArrayList<String> Sugerencias;
+	private Empleado usuarioActual ;
+	
+	public SistemaBoardGameCafe() {
+		
+	}
+	
+	public void cargarDatos() {  // carga de datos falta percistencia
+		
+	}
+	
+	public void guardarDatos() {  // guardar datos falta percistencia 
+		
+	}
+	
+	public boolean inciarSesion(String login, String password) {
+		try {
+			this.empleados.get(login+password);
+			return true;
+		}
+		catch(Exception e) {
+			return false;
+		}
+		
+
+	}
+	
+	public boolean cerrarSesion() {
+		this.usuarioActual = null;
+		return true;
+	}
+	
+	public boolean procesarPrestamo(String idJuego, int idMesa, boolean esCliente, Cliente cliente 
+			, Empleado empleado , juegoMesa juego,  LocalDateTime inicio) {
+		if (esCliente) {
+
+			PrestamoCliente prestamo = new PrestamoCliente(this.inventario.get(idJuego),inicio, null,cliente  ); // aca el null esta por que no tiene fecha de entrega aun
+			try {
+				if(PrestamoCliente.sonAptos(this.mesas.get(idMesa)) &&  !this.inventario.get(idJuego).getEstado()) {
+					this.mesas.get(idMesa).AgregarPrestamo(prestamo);
+					this.inventario.get(idJuego).cambiarEstado(true);
+					this.historailPrestamosClientes.put(idJuego, prestamo);
+					
+					return true;
+				}else {
+					return false;
+				}
+				
+			} catch(Exception e) {
+				return false;
+			}
+			
+		}else {
+			PrestamoEmpleado prestano = new PrestamoEmpleado(this.inventario.get(idJuego),inicio,null,empleado);// aca el null esta por que no tiene fecha de entrega aun
+			try {
+				if(PrestamoEmpleado.sonAptos(null) &&  !this.inventario.get(idJuego).getEstado() ){
+					this.historailPrestamosEmpleados.put(idJuego, prestano);
+					this.inventario.get(idJuego).cambiarEstado(true);
+					return true;
+				} else {
+					return false;
+				}
+			}catch(Exception e) {
+				return false;
+			}
+		}
+		return true;
+		
+	
+	}
+	
+	public void limpiarMesa(LocalDateTime fin, Mesa mesa) {
+		ArrayList<String> codigos = mesa.liberarMesa();
+		for(String i: codigos) {
+			this.historailPrestamosClientes.get(i).cambiarEstado(false);
+		}
+		
+	}
+	
+	public Venta registrarVenta(int idMesa) {
+		Venta venta = new Venta()
+	}
+	
 }
