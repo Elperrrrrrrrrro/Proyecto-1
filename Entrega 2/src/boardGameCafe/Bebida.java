94bde3b7dfb12db7723f0ea63a49b3ee5d3bc6ea
Elperrrrrrrrrro <dg.arias@uniandes.edu.co>
2026-04-12 15:32:32 -0500

    implementacon producotmenu y sus hijos y parte de mesa

--- Entrega 2/src/boardGameCafe/Bebida.java
+++ Entrega 2/src/boardGameCafe/Bebida.java
@@ -1,5 +1,33 @@
 package boardGameCafe;
 
-public class Bebida {
+public class Bebida extends ProductoMenu{
+private boolean esAlcoholica ;
+private boolean temperatura; 
 
+public Bebida(String nombre, String codigo  , boolean alcoholica, boolean temperatura, double precioBase,String codigoItem,String descripcion) {
+	super( nombre,  precioBase, codigoItem, descripcion);
+	this.esAlcoholica=alcoholica;
+	this.temperatura=temperatura;
+	
+}	
+
+
+
+public boolean esApto(Mesa mesa) {
+		if (mesa.isHayMenores()) {
+			return false;
+		}
+		// aca falta implementar el get juego de producto y el get categoria de juego mesa 
+		// por eso marca error
+		if (this.temperatura) {
+			for (Prestamo i : mesa.getPrestamoActicos()) {
+				if ( "Accion".equals(i.getJuego().getCategoria()){
+					return false;
+				}
+				
+			}
+		}
+		
+		return true;
+}
 }
--- Entrega 2/src/boardGameCafe/Mesa.java
+++ Entrega 2/src/boardGameCafe/Mesa.java
@@ -1,5 +1,34 @@
 package boardGameCafe;
-
+import java.util.*;
 public class Mesa {
-
+	private int numeroMesa;
+	private Cliente clienteActual;
+	private ArrayList<ProductoMenu> preidoActual;
+	private ArrayList<Prestamo >prestamoActicos;
+	private int cantidadPersonas;
+	private boolean hayMenores;
+	private ArrayList<String> alergenos;
+	public int getNumeroMesa() {
+		return numeroMesa;
+	}
+	public Cliente getClienteActual() {
+		return clienteActual;
+	}
+	public ArrayList<ProductoMenu> getPreidoActual() {
+		return preidoActual;
+	}
+	public ArrayList<Prestamo> getPrestamoActicos() {
+		return prestamoActicos;
+	}
+	public int getCantidadPersonas() {
+		return cantidadPersonas;
+	}
+	public boolean isHayMenores() {
+		return hayMenores;
+	}
+	public ArrayList<String> getAlergenos() {
+		return alergenos;
+	}
+	
+	
 }
