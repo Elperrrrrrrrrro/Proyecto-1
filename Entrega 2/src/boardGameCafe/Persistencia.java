59d34c414772e6cd21e7ea1d0d11eda9424a5ca8
LauraAcosta77 <ld.acostas1@uniandes.edu.co>
2026-04-12 22:34:11 -0500

    Prueba persistencia

--- Entrega 2/src/boardGameCafe/Persistencia.java
+++ Entrega 2/src/boardGameCafe/Persistencia.java
@@ -1,9 +1,31 @@
 package boardGameCafe;
-
+import java.io.*;
 public class Persistencia {
 
-	public Persistencia() {
-		// TODO Auto-generated constructor stub
-	}
+		    private static final String ARCHIVO = "sistema.dat";
 
+		    public static void guardarSistema(SistemaBoardGameCafe sistema) {
+		        try (ObjectOutputStream out = new ObjectOutputStream(
+		                new FileOutputStream(ARCHIVO))) {
+
+		            out.writeObject(sistema);
+
+		            System.out.println(" Sistema guardado");
+
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+		    }
+
+		    public static SistemaBoardGameCafe cargarSistema() {
+		        try (ObjectInputStream in = new ObjectInputStream(
+		                new FileInputStream(ARCHIVO))) {
+
+		            return (SistemaBoardGameCafe) in.readObject();
+
+		        } catch (Exception e) {
+		            System.out.println("Sistema nuevo creado");
+		            return new SistemaBoardGameCafe();
+		        }
+		    }
 }
