56b39e52171472bfa4c70bc878bb5d9029ee418a
LauraAcosta77 <ld.acostas1@uniandes.edu.co>
2026-04-12 19:30:56 -0500

    Merge lau

--- Entrega 2/src/boardGameCafe/Prestamo.java
+++ Entrega 2/src/boardGameCafe/Prestamo.java
@@ -1,5 +1,37 @@
 package boardGameCafe;
 
-public class Prestamo {
+import java.time.LocalDateTime;
 
+public abstract class Prestamo {
+
+    protected String idPrestamo;
+    protected juegoMesa juego;
+    protected LocalDateTime fechaPrestamo;
+    protected LocalDateTime fechaDevolucion;
+
+    public Prestamo(String idPrestamo, juegoMesa juego,
+                    LocalDateTime fechaPrestamo, LocalDateTime fechaDevolucion) {
+        this.idPrestamo = idPrestamo;
+        this.juego = juego;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public abstract boolean sonAptos(Mesa mesa);
+
+    public String getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public juegoMesa getJuego() {
+        return juego;
+    }
+
+    public LocalDateTime getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public LocalDateTime getFechaDevolucion() {
+        return fechaDevolucion;
+    }
 }
--- Entrega 2/src/boardGameCafe/PrestamoEmpleado.java
+++ Entrega 2/src/boardGameCafe/PrestamoEmpleado.java
@@ -1,5 +1,32 @@
 package boardGameCafe;
+import java.time.LocalDateTime;
+import java.util.Map;
 
-public class PrestamoEmpleado {
+public class PrestamoEmpleado extends Prestamo {
 
+    private Empleado empleado;
+    private Map<String, Turno> turnos;
+
+    public PrestamoEmpleado(String idPrestamo, juegoMesa juego, LocalDateTime inicio, LocalDateTime entrega,
+                            Empleado empleado, Map<String, Turno> turnos) {
+        super(idPrestamo,juego, inicio, entrega);
+        this.empleado = empleado;
+        this.turnos = turnos;
+    }
+
+    @Override
+    public boolean sonAptos(Mesa mesa) {
+        // 1. ver si el juego esta disponible
+        if (juego.getEstado()) {
+            return false;
+        }
+        // ver que no este en turno
+        for (Turno t : turnos.values()) {
+            if (t.getEmpleadosAsignados().contains(empleado)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
